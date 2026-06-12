934ceda49a6ce3e3fcfa97bd3b280286ca8bd46e
DaviSchulz <davischulz321@gmail.com>
2026-06-12 13:59:31 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,62 @@
+const encontro = new Date("2026-06-13T12:00:00");
+
+function atualizar() {
+
+    const agora = new Date();
+
+    const diferenca = encontro - agora;
+
+    const dias =
+        Math.floor(diferenca / (1000*60*60*24));
+
+    const horas =
+        Math.floor((diferenca/(1000*60*60))%24);
+
+    const minutos =
+        Math.floor((diferenca/(1000*60))%60);
+
+    const segundos =
+        Math.floor((diferenca/1000)%60);
+
+    document.getElementById("dias").innerHTML = dias;
+    document.getElementById("horas").innerHTML = horas;
+    document.getElementById("minutos").innerHTML = minutos;
+    document.getElementById("segundos").innerHTML = segundos;
+}
+
+setInterval(atualizar,1000);
+
+atualizar();
+
+function criarCoracao(){
+
+    const heart = document.createElement("div");
+
+    heart.innerHTML = "❤️";
+
+    heart.style.position = "fixed";
+    heart.style.left = Math.random()*100+"vw";
+    heart.style.bottom = "-50px";
+    heart.style.fontSize = (Math.random()*20+15)+"px";
+
+    document.body.appendChild(heart);
+
+    const anim = setInterval(()=>{
+
+        let atual =
+        parseInt(heart.style.bottom);
+
+        heart.style.bottom =
+        atual + 3 + "px";
+
+        if(atual > window.innerHeight){
+
+            clearInterval(anim);
+            heart.remove();
+
+        }
+
+    },20);
+}
+
+setInterval(criarCoracao,500);
